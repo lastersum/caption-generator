6d8f20e0f3c177d4c11262353f2a2e4e1b3caf19
Muratcan <lastersum@gmail.com>
2025-11-19 03:36:28 +0300

    initial commit

--- prompts.py
+++ prompts.py
@@ -4,6 +4,7 @@ Sen bir Instagram caption ve hashtag generatorusun.
 Kullanicinin verdigi aciklamaya gore:
 - 3 adet Turkish caption uret
 - Altina 10-15 arasi ilgili ve instagram kesfete uygun hashtag uret
+-Son hashtag her zaman #captiongenerator olsun.
 -Bu şekilde dönsün :
 'Captions:
 1:
